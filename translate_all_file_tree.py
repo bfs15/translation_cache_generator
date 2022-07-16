b7db471c647099f06a36d1142b82b18a9ded612f
Bruno FS <bruno.serbena@gmail.com>
2022-07-16 20:31:48 -0300

    added command arguments to translate scripts

--- translate_all_file_tree.py
+++ translate_all_file_tree.py
@@ -1,8 +1,20 @@
 
-import translation_cache_generator
+import sys
 from pathlib import Path
 
-texts = []
-texts = Path(".").glob("**/*")
+import translation_cache_generator
 
-translation_cache_generator.main(texts)
+def main():
+	if len(sys.argv) != 2:
+		print("Usage: python translation_cache_generator.py [path_to_directory_yourd_like_the_tree_translated]")
+		return
+		
+	directoryPath = sys.argv[1]
+
+	texts = []
+	texts = Path(directoryPath).glob("**/*")
+
+	translation_cache_generator.main(texts)
+
+if __name__ == "__main__":
+	main()
--- translate_all_file_tree_mmd.py
+++ translate_all_file_tree_mmd.py
@@ -1,20 +1,29 @@
 
-import translation_cache_generator
+import sys
 from pathlib import Path
 
+import translation_cache_generator
 
-texts = []
-texts = Path(".").glob("**/*")
+def main():
+	if len(sys.argv) != 2:
+		print("Usage: python translation_cache_generator.py [path_to_directory_yourd_like_the_tree_translated]")
+		return
+		
+	directoryPath = sys.argv[1]
+	texts = []
+	texts = Path(directoryPath).glob("**/*")
 
-def dir_are_there_files(d, suffix):
-	bool(list(Path(d).glob(f"**/{suffix}")))
+	def dir_are_there_files(d, suffix):
+		bool(list(Path(d).glob(f"**/{suffix}")))
 
-def should_translate(p):
-	return (p.is_dir() and not dir_are_there_files(p, "*.pmx")) or (
-		not p.is_dir() and p.suffix == ".pmx"
-	)  # and (p.suffix not in [".png", ".jpg", ".jpeg", ".bmp"])
+	def should_translate(p):
+		return (p.is_dir() and not dir_are_there_files(p, "*.pmx")) or (
+			not p.is_dir() and p.suffix == ".pmx"
+		)  # and (p.suffix not in [".png", ".jpg", ".jpeg", ".bmp"])
 
-texts = (str(Path(p).name) for p in texts if should_translate(p))
+	texts = (str(Path(p).name) for p in texts if should_translate(p))
 
+	translation_cache_generator.main(texts)
 
-translation_cache_generator.main(texts)
+if __name__ == "__main__":
+	main()
